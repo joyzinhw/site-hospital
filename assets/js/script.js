9ba4189e776d590cfeed17838aad1af9a45049a3
joyce moura <mileniorapper@gmail.com>
2025-02-05 12:49:21 -0300

    add: ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,6 @@ navbarHideBtn.addEventListener('click', function(){
     navbarCollapseDiv.classList.remove('navbar-show');
 });
 
-// changing search icon image on window resize
-window.addEventListener('resize', changeSearchIcon);
-function changeSearchIcon(){
-    let winSize = window.matchMedia("(min-width: 1200px)");
-    if(winSize.matches){
-        document.querySelector('.search-icon img').src = "images/search-icon.png";
-    } else {
-        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
-    }
-}
-changeSearchIcon();
 
 // stopping all animation and transition
 let resizeTimer;
