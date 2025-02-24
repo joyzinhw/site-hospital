8e781ac7260a60dccb656cc0e124899a0eb0a1fc
Joyce Moura <joyzinhw@gmail.com>
2025-02-24 07:58:44 -0300

    add: chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,18 @@ navigator.serviceWorker.addEventListener('controllerchange', () => {
       refreshing = true;
   }
 });
+
+
+
+
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67bc48d5578a2b1907cfaedf/1ikrnp10h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
