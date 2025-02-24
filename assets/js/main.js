fd3c08d364e4a3a372fc374770c7dcdc5a9f1734
Joyce Moura <joyzinhw@gmail.com>
2025-02-24 09:03:29 -0300

    add: chat ajuste

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,10 +60,6 @@ navigator.serviceWorker.addEventListener('controllerchange', () => {
 });
 
 
-
-
-
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
